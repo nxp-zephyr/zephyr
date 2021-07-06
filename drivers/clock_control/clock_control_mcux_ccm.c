92ba87b742ccce883551a05fab37d1bcc8c32f21
Jiafei Pan <Jiafei.Pan@nxp.com>
2021-07-06 17:52:12 +0800

    clock: mcux_ccm: add gpt ipg clock
    GPT IPG clock is using GPTx_CLK_ROOT.
    
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>

--- drivers/clock_control/clock_control_mcux_ccm.c
+++ drivers/clock_control/clock_control_mcux_ccm.c
@@ -201,6 +201,14 @@ static int mcux_ccm_get_subsys_rate(const struct device *dev,
 	case IMX_CCM_GPT_CLK:
 		*rate = CLOCK_GetFreq(kCLOCK_PerClk);
 		break;
+	case IMX_CCM_GPT_IPG_CLK:
+	{
+		uint32_t mux = CLOCK_GetRootMux(kCLOCK_RootGpt1);
+		if (mux == 0)
+			*rate = OSC24M_CLK_FREQ;
+		else
+			*rate = 0;
+	} break;
 #endif
 
 #ifdef CONFIG_COUNTER_MCUX_QTMR
