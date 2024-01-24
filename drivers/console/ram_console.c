b0c01c924353be97c649fab4089d60b85e9175a1
Hou Zhiqiang <Zhiqiang.Hou@nxp.com>
2024-01-24 15:18:25 +0800

    drivers: ram_console: add option to link console buffer to given section
    Currently, the ram_console buffer is defined as a global var, its
    address is determined during the building and may be changed when
    code update. This is not a problem if the ram_console is just used
    in debug purpose.
    
    While in the heterogeneous SoCs, there can be multiple MPU Cores
    and several MCU cores, it can run multiple OS/bare-metal instances
    on these cores, but the UART ports may be not enough, so the
    ram_console can be leveraged. To make it easy to use, it's better
    make the console buffer fixed and predefined.
    
    This patch adds a option to link the console buffer to a given
    section, through the "zephyr,memory-region" device tree node, then
    the address can be known from the device tree node and easy to
    check from other cores running Linux/U-Boot.
    
    The lable of the "zephyr,memory-region" device tree node must be
    'ram_console' as following:
    
    	ram_console: memory@93d00000 {
    		compatible = "zephyr,memory-region";
    		reg = <0x93d00000 DT_SIZE_K(4)>;
    		zephyr,memory-region = "RAM_CONSOLE";
    	};
    
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

--- drivers/console/ram_console.c
+++ drivers/console/ram_console.c
@@ -2,6 +2,7 @@
 
 /*
  * Copyright (c) 2015 Intel Corporation
+ * Copyright 2024 NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -11,23 +12,35 @@
 #include <zephyr/sys/printk.h>
 #include <zephyr/device.h>
 #include <zephyr/init.h>
+#include <zephyr/linker/devicetree_regions.h>
 
 extern void __printk_hook_install(int (*fn)(int));
 extern void __stdout_hook_install(int (*fn)(int));
 
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(ram_console), okay)
+char ram_console[DT_REG_SIZE(DT_NODELABEL(ram_console))] __attribute__((__section__(LINKER_DT_NODE_REGION_NAME(DT_NODELABEL(ram_console)))));
+#else
 /* Extra byte to ensure we're always NULL-terminated */
 char ram_console[CONFIG_RAM_CONSOLE_BUFFER_SIZE + 1];
+#endif
+
 static int pos;
+static unsigned int size;
 
 static int ram_console_out(int character)
 {
 	ram_console[pos] = (char)character;
-	pos = (pos + 1) % CONFIG_RAM_CONSOLE_BUFFER_SIZE;
+	pos = (pos + 1) % size;
 	return character;
 }
 
 static int ram_console_init(void)
 {
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(ram_console), okay)
+	size = DT_REG_SIZE(DT_NODELABEL(ram_console)) - 1;
+#else
+	size = CONFIG_RAM_CONSOLE_BUFFER_SIZE;
+#endif
 	__printk_hook_install(ram_console_out);
 	__stdout_hook_install(ram_console_out);
 
