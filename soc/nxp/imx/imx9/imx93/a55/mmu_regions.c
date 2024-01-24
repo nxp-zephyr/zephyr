bc904c51825ad7e80d1be0f428822e2f3f173b6a
Zineb El Azzouzi <zineb.elazzouzi@nxp.com>
2024-01-24 16:58:34 +0100

    imx93: add LPI2C4 and GPIO2 mmu mappings
    Signed-off-by: Zineb El Azzouzi <zineb.elazzouzi@nxp.com>

--- soc/nxp/imx/imx9/imx93/a55/mmu_regions.c
+++ soc/nxp/imx/imx9/imx93/a55/mmu_regions.c
@@ -77,6 +77,10 @@ static const struct arm_mmu_region mmu_regions[] = {
 			      DT_REG_SIZE(DT_INST(0, nxp_imx_lpi2c)),
 			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),
 
+	MMU_REGION_FLAT_ENTRY("LPI2C4", DT_REG_ADDR(DT_NODELABEL(lpi2c4)),
+			      DT_REG_SIZE(DT_NODELABEL(lpi2c4)),
+			      MT_DEVICE_nGnRE | MT_P_RW_U_RW | MT_NS),
+
 	MMU_REGION_FLAT_ENTRY("SAI1", DT_REG_ADDR(DT_INST(0, nxp_imx_sai)),
 			      DT_REG_SIZE(DT_INST(0, nxp_imx_sai)),
 			      MT_DEVICE_nGnRE | MT_P_RW_U_NA | MT_NS),
@@ -88,6 +92,10 @@ static const struct arm_mmu_region mmu_regions[] = {
 	MMU_REGION_FLAT_ENTRY("SAI3", DT_REG_ADDR(DT_INST(2, nxp_imx_sai)),
 			      DT_REG_SIZE(DT_INST(2, nxp_imx_sai)),
 			      MT_DEVICE_nGnRE | MT_P_RW_U_NA | MT_NS),
+
+	MMU_REGION_FLAT_ENTRY("GPIO2", DT_REG_ADDR(DT_NODELABEL(gpio2)),
+			      DT_REG_SIZE(DT_NODELABEL(gpio2)),
+			      MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_NS),
 };
 
 const struct arm_mmu_config mmu_config = {
