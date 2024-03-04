055ed447a7c372903f143a79c081f108aaaad53a
Hou Zhiqiang <Zhiqiang.Hou@nxp.com>
2024-03-04 21:39:43 +0800

    driver: ram_console: add RAM console header at the first 64B memory
    Add some header information at the first 64 bytes of ram console memory,
    it can be used by Linux ram console dump tool: ram_console_dump.
    
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>

--- drivers/console/ram_console.c
+++ drivers/console/ram_console.c
@@ -8,11 +8,13 @@
  */
 
 
+#include <string.h>
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 #include <zephyr/device.h>
 #include <zephyr/init.h>
 #include <zephyr/linker/devicetree_regions.h>
+#include <zephyr/drivers/console/ram_console.h>
 
 #ifdef CONFIG_RAM_CONSOLE_BUFFER_SECTION
 #if !DT_HAS_CHOSEN(zephyr_ram_console)
@@ -32,27 +34,35 @@ extern void __stdout_hook_install(int (*fn)(int));
 
 char ram_console_buf[CONFIG_RAM_CONSOLE_BUFFER_SIZE] RAM_CONSOLE_BUF_ATTR;
 char *ram_console;
-static int pos;
+static struct ram_console_header *header;
 
 static int ram_console_out(int character)
 {
-	ram_console[pos] = (char)character;
-	/* Leave one byte to ensure we're always NULL-terminated */
-	pos = (pos + 1) % (CONFIG_RAM_CONSOLE_BUFFER_SIZE - 1);
+	header->buf_addr[header->pos] = (char)character;
+	header->pos = (header->pos + 1) % header->buf_size;
 	return character;
 }
 
 static int ram_console_init(void)
 {
+	unsigned int size = CONFIG_RAM_CONSOLE_BUFFER_SIZE - 1;
+
 #ifdef CONFIG_RAM_CONSOLE_BUFFER_SECTION
 	mm_reg_t ram_console_va;
 
 	device_map((mm_reg_t *)&ram_console_va, DT_REG_ADDR(DT_CHOSEN(zephyr_ram_console)),
-		   CONFIG_RAM_CONSOLE_BUFFER_SIZE, K_MEM_CACHE_NONE | K_MEM_DIRECT_MAP);
-	ram_console = (char *)ram_console_va,
+		   size, K_MEM_CACHE_NONE | K_MEM_DIRECT_MAP);
+	ram_console = (char *)ram_console_va;
 #else
 	ram_console = ram_console_buf,
 #endif
+	memset(ram_console, 0, size + 1);
+	header = (struct ram_console_header *)ram_console;
+	strcpy(header->flag_string, RAM_CONSOLE_HEAD_STR);
+	header->buf_addr = (char *)(ram_console + RAM_CONSOLE_HEAD_SIZE);
+	header->buf_size = size - RAM_CONSOLE_HEAD_SIZE;
+	header->pos = 0;
+
 	__printk_hook_install(ram_console_out);
 	__stdout_hook_install(ram_console_out);
 
