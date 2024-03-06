fenderdigital/zephyr-imx8

clock: mcux_ccm: add gpt ipg clock
GPT IPG clock is using GPTx_CLK_ROOT.

Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>

include/dt-bindings/clock/imx_ccm.h
@@ -45,4 +45,6 @@
 
 #define IMX_CCM_PWM_CLK			0x0C00UL
 
+#define IMX_CCM_GPT_IPG_CLK		0x0D00UL
+
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_IMX_CCM_H_ */

nxp-zephyr/zephyr

include/zephyr/dt-bindings/clock/imx_ccm.h
@@ -61,4 +61,6 @@
 #define IMX_CCM_TPM5_CLK                0x0F04UL
 #define IMX_CCM_TPM6_CLK                0x0F05UL
 
+#define IMX_CCM_GPT_IPG_CLK		0x0E00UL
+
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_IMX_CCM_H_ */