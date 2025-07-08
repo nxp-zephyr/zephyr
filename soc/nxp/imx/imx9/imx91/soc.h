2592b115669e195b351b1e8e995b6fdb8332ee7f
Lei Xu <lei.xu@nxp.com>
2025-07-08 00:29:32 -0700

    soc: imx91: add empty soc.h
    Some drivers need header file soc.h, according to Zephyr SoC Porting Guide
    soc.h must be provided for each SoC, so created an empty one.
    
    Signed-off-by: Lei Xu <lei.xu@nxp.com>

---
+++ soc/nxp/imx/imx9/imx91/soc.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2025 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _SOC_NXP_IMX_IMX91_SOC_H_
+#define _SOC_NXP_IMX_IMX91_SOC_H_
+
+
+#endif /* _SOC_NXP_IMX_IMX91_SOC_H_ */
